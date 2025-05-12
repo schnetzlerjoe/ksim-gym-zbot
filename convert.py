1fa4bd8c0ffc5d1a86747b411c1529c6b961f30e
alik-git <ali@kscale.dev>
2025-05-11 20:41:23 -0700

    kinfer convert

--- convert.py
+++ convert.py
@@ -5,10 +5,14 @@ from pathlib import Path
 from typing import Callable
 
 import jax
+import jax.numpy as jnp
+import ksim
 from jaxtyping import Array
-from xax.nn.export import export as xax_export
+from kinfer.export.jax import export_fn
+from kinfer.export.serialize import pack
 
-from train import NUM_ACTOR_INPUTS, Model, ZbotWalkingTask
+from train import ZbotWalkingTask, Model
+
 
 
 def make_export_model(model: Model) -> Callable:
@@ -33,18 +37,65 @@ def main() -> None:
 
     task: ZbotWalkingTask = ZbotWalkingTask.load_task(ckpt_path)
     model: Model = task.load_ckpt(ckpt_path, part="model")[0]
-    model_fn = make_export_model(model)
 
-    input_shapes = [
-        (NUM_ACTOR_INPUTS,),
-        (
-            task.config.depth,
-            task.config.hidden_size,
-        ),
-    ]
+    # Loads the Mujoco model and gets the joint names.
+    mujoco_model = task.get_mujoco_model()
+    joint_names = ksim.get_joint_names_in_order(mujoco_model)[1:]  # Removes the root joint.
 
-    xax_export(model_fn, input_shapes, args.output_path)
+    # Constant values.
+    carry_shape = (task.config.depth, task.config.hidden_size)
+    num_joints = len(joint_names)
 
+    @jax.jit
+    def init_fn() -> Array:
+        return jnp.zeros(carry_shape)
 
+    @jax.jit
+    def step_fn(
+        joint_angles: Array,
+        joint_angular_velocities: Array,
+        projected_gravity: Array,
+        # accelerometer: Array,
+        # gyroscope: Array,
+        carry: Array,
+    ) -> tuple[Array, Array]:
+        obs = jnp.concatenate(
+            [
+                joint_angles,
+                joint_angular_velocities,
+                projected_gravity,
+                # accelerometer,
+                # gyroscope,
+            ],
+            axis=-1,
+        )
+        dist, carry = model.actor.forward(obs, carry)
+        return dist.mode(), carry
+
+    init_onnx = export_fn(
+        model=init_fn,
+        num_joints=num_joints,
+        carry_shape=carry_shape,
+    )
+
+    step_onnx = export_fn(
+        model=step_fn,
+        num_joints=num_joints,
+        carry_shape=carry_shape,
+    )
+
+    kinfer_model = pack(
+        init_fn=init_onnx,
+        step_fn=step_onnx,
+        joint_names=joint_names,
+        carry_shape=carry_shape,
+    )
+
+    # Saves the resulting model.
+    (output_path := Path(args.output_path)).parent.mkdir(parents=True, exist_ok=True)
+    print(f"Saving model to {output_path}")
+    with open(output_path, "wb") as f:
+        f.write(kinfer_model)
+        
 if __name__ == "__main__":
     main()
